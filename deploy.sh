daaebd9f4e3a8cf9dde7668ed6543c10462169f4
smallzip <13112901815@163.com>
2022-03-30 23:58:42 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 containerName="hongrunxing-api"
 
-docker inspect -f '{{.Name}}' containerName
+docker inspect -f '{{.Name}}' $containerName
 
 echo "搜索结果：$?"
 
 if [ $? == 0 ];
 then
     echo "hongrunxing-api容器已存在"
-    docker container stop containerName
-    docker container rm containerName
-    docker rmi containerName node:14-alpine node:14
+    docker container stop $containerName
+    docker container rm $containerName
+    docker rmi $containerName node:14-alpine node:14
 else
     echo "$containerName容器不存在，开始执行构建"
 fi
@@ -19,3 +19,9 @@ fi
 docker build -t hongrunxing-api .
 
 docker run -d -p 7001:7001 --name hongrunxing-api hongrunxing-api
+
+# 删除不用等容器和镜像
+
+docker container prune -f
+
+docker image prune -a -f
